c34807e80056d43967a1661a989f6623dba7daee
Mr.L <liaowz@ijovo.com>
2019-11-15 22:16:35 +0800

    update ANSI to UTF8

---
+++ day02/code/01_常量/constan1.java
@@ -0,0 +1,9 @@
+class Constant1{
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+        System.out.println(28);
+        System.out.println(12.123);
+        System.out.println('k');
+        System.out.println(true);
+    }
+}
